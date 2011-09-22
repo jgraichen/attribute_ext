aec86553e637e0d16944bc5c844f4e354c916c1d
Jan Graichen <jan.graichen@altimos.de>
2011-09-22 15:22:44 +0200

    Move init code to rails/init.rb

--- init.rb
+++ init.rb
@@ -1,5 +1,2 @@
 
-require 'attribute_ext'
-
-ActiveRecord::Base.send :include, AttributeExt::HiddenAttributes
-ActiveRecord::Base.send :include, AttributeExt::SafeAttributes
+require File.dirname(__FILE__) + '/rails/init.rb'
