61202a586899bb850d06706824f3ccd729aa920d
Jan Graichen <jan.graichen@altimos.de>
2011-09-22 15:16:39 +0200

    Improve code and add as condition to SafeAttributes.

--- lib/attribute_ext/safe_attributes.rb
+++ lib/attribute_ext/safe_attributes.rb
@@ -2,6 +2,7 @@ module AttributeExt
   module SafeAttributes
     def self.included(base)
       base.extend(ClassMethods)
+      base.alias_method_chain :mass_assignment_authorizer, :safe_attrs
     end
 
     module ClassMethods
@@ -11,26 +12,34 @@ module AttributeExt
           @safe_attributes
         else
           options = attrs.last.is_a?(Hash) ? attrs.pop : {}
-          @safe_attributes << [attrs, options]
+          @safe_attributes << [attrs, safe_attributes_opts(options)]
         end
+      end
+      
+      private
+      def safe_attributes_opts(options)
+        opts = { :as => [] }
+        opts[:as]    += options[:as].is_a?(Array) ? options[:as] : [options[:as]] if options[:as]
+        opts[:if]     = options[:if] if options[:if].is_a?(Proc)
+        opts[:unless] = options[:unless] if options[:unless].is_a?(Proc)
+        opts
       end
     end
 
-    def mass_assignment_authorizer(role = nil)
-      if role.nil?
-        super + safe_attribute_names(:default)
-      else
-        super(role) + safe_attribute_names(role)
-      end
+    def mass_assignment_authorizer_with_safe_attrs(role = nil)
+      attrs = role.nil? ? mass_assignment_authorizer_without_safe_attrs : mass_assignment_authorizer_without_safe_attrs(role)
+      attrs += safe_attribute_names(role ? role : :default)
+      attrs.uniq
     end
 
     def safe_attribute_names(role = :default)
       names = []
       self.class.safe_attributes.collect do |attrs, options|
-        if (options[:if].nil? || safe_attrs_call_block(options[:if], role)) &&        # if
-            (options[:unless].nil? || !safe_attrs_call_block(options[:unless], role)) # unless
-          names += attrs.collect(&:to_s)
-        end
+        next unless options[:as].empty? or options[:as].include?(role)
+        next unless options[:if].nil? or safe_attrs_call_block(options[:if], role)
+        next unless options[:unless].nil? or !safe_attrs_call_block(options[:unless], role)
+
+        names += attrs.collect(&:to_s)
       end
       names.uniq
     end
