e03feb8f58a2f4d51025c2cc5cb5002549b0b516
Jan Graichen <jan.graichen@altimos.de>
2012-03-06 14:38:18 +0100

    Improve spec helper and add sqlite3 in-memory support.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,14 @@
 
-require File.dirname(__FILE__) + '/support/fake_environment'
-require File.dirname(__FILE__) + '/../init'
-require File.dirname(__FILE__) + '/support/stub'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../dummy/config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+Dir[File.expand_path("../support/**/*.rb", __FILE__)].each {|f| require f}
 
 RSpec.configure do |config|
-  config.mock_with :rspec
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+
+  initialize_in_memory_database if in_memory_database?
 end
